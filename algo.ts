31e081192e6c90fdcee2d5decd099ccea03a8747
Joel-Lavia <joeldisidi8@gmail.com>
2024-10-16 09:43:06 +0100

    feat:resolve time convertor

--- algo.ts
+++ algo.ts
@@ -67,15 +67,80 @@
 // const numbers: number = Math.max(...candale);
 //   const max: number[] = [];
 
-
 // for (let i = 0; i < candale.length; i++) {
 //   const element = candale[i];
 // if (numbers==element) {
 // max.push(element);
 // }
-  
+
 // }
 //   return max.length;
 // }
 
 // console.log(birthdayCakeCandles([3, 2, 3, 3]));
+
+// function timeConversion(s: string): string {
+//   // Write your code here
+//   const hoursMilitary: string[] = [
+//     "00",
+//     "01",
+//     "02",
+//     "03",
+//     "04",
+//     "05",
+//     "06",
+//     "07",
+//     "08",
+//     "09",
+//     "10",
+//     "11",
+//     "12",
+//     "13",
+//     "14",
+//     "15",
+//     "16",
+//     "17",
+//     "18",
+//     "19",
+//     "20",
+//     "21",
+//     "22",
+//     "23",
+//   ];
+
+//   let hour: string = "";
+//   let heure: string = "";
+
+//   if (s.includes("PM")) {
+//     for (let i = 0; i < hoursMilitary.length; i++) {
+//       const element = hoursMilitary[i];
+//       if (s.substring(0, 2) == element) {
+//         const num: number = parseInt(element, 10);
+//         const stringNumber: number = num + 12;
+//         hour = s.replace(element, stringNumber.toString());
+
+//         if (s.substring(0, 2) == "12") {
+//           hour = s.replace(element, "12");
+//         }
+//       }
+//     }
+//     heure = hour.replace("PM", "");
+//   } else if (s.includes("AM")) {
+//     for (let i = 0; i < hoursMilitary.length; i++) {
+//       const element = hoursMilitary[i];
+//       if (s.substring(0, 2) == element) {
+//         hour = s.replace(element, element);
+//         if (s.substring(0, 2) == "12") {
+//           hour = s.replace(element, "00");
+//         }
+//       }
+//     }
+//     heure = hour.replace("AM", "");
+//   }
+
+//   return heure;
+// }
+
+// console.log(timeConversion("12:05:45AM"));
+// console.log(timeConversion("08:05:45PM"));
+
